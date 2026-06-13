979338255a2c5ec40ff90e789f5187785dc9d150
AnasRezk <anasrizk01@gmail.com>
2026-06-13 16:45:19 +0200

    Add CRUD endpoints for combo management in server.js

--- server.js
+++ server.js
@@ -5,6 +5,28 @@ const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
 server.use(middlewares);
+
+const rewriter = jsonServer.rewriter({
+  '/GetAllCombos': '/combos',
+  '/GetComboById/:id': '/combos/:id'
+});
+server.use(rewriter);
+
+server.post('/CreateCombo', (req, res, next) => {
+  req.url = '/combos';
+  next();
+});
+
+server.put('/UpdateCombo/:id', (req, res, next) => {
+  req.url = `/combos/${req.params.id}`;
+  next();
+});
+
+server.delete('/DeleteCombo/:id', (req, res, next) => {
+  req.url = `/combos/${req.params.id}`;
+  next();
+});
+
 server.use(router);
 
 server.listen(port);
